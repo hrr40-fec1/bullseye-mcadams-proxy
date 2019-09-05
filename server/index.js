a83800a7b961ab4608d065b44bab43f42f2b7156
minmaxdata <kemcadams@gmail.com>
2019-09-04 20:37:10 -0700

    edits to get product id

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
@@ -6,8 +7,26 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 app.use(express.static(path.join(__dirname, '/../public')));
 app.get('/', (req, res) => res.send('OK'));
-app.use(proxy('/api/product/', { target: 'http://localhost:3004' }));
-app.use(proxy('/api/checkout/', { target: 'http://localhost:3002' }));
-app.use(proxy('/api/', { target: 'http://localhost:3001' }));
+
+const updateQueryStringParameter = (pth, key, value) => {
+  const re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+  const separator = pth.indexOf('?') !== -1 ? '&' : '?';
+  if (pth.match(re)) {
+    return pth.replace(re, '$1' + key + '=' + value + '$2');
+  }
+  return pth + separator + key + '=' + value;
+};
+
+app.use(proxy('/api/product/', {
+  target: 'http://localhost:3004/',
+  pathRewrite: (pth, req) => {
+    const id = req.headers.referer.split('?productId=');
+    let newPath = pth;
+    newPath = updateQueryStringParameter(newPath, 'productId', id[1]);
+    return newPath;
+  },
+}));
+app.use(proxy('http://localhost:3002/api/checkout/'));
+app.use(proxy('http://localhost:3001/api/'));
 
 module.exports = app;
